3785572c26d309f1e342a71b23e0d7a2cd7c61d4
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2023-09-13 17:05:32 +0700

    add listMenu

--- Pegawai Restoran/src/Fitur/ListMenu.java
+++ Pegawai Restoran/src/Fitur/ListMenu.java
@@ -1,3 +1,24 @@
+package Fitur;
+import java.util.Scanner;
+
 public class ListMenu {
-    
+    public static void main(String[] args) {
+        // Scanner
+        Scanner p1 = new Scanner(System.in);
+        
+        // Print line
+        System.out.println("=========================================");
+        System.out.println("           MENU SISTEM KARYAWAN          ");
+        System.out.println("=========================================");
+        System.out.println("1. List Data Karyawan");
+        System.out.println("2. Tambah Data Karyawan");
+        System.out.println("3. Ubah Data Karyawan");
+        System.out.println("4. Hapus Data Karyawan");
+        System.out.println("5. Sistem Gaji");
+        System.out.println("=========================================");
+
+        System.out.println("  Pilih menu yang anda inginkan (1-5) : ");
+        int pilih = p1.nextInt();
+        
+    }
 }
