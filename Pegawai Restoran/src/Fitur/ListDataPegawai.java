79c63cc1c229e7d0d308afbdf6ed2bf33083ad0a
Duta Kurnia A <dutaka2945@gmail.com>
2023-09-13 20:57:52 +0700

    add List Data Pegawai

--- Pegawai Restoran/src/Fitur/ListDataPegawai.java
+++ Pegawai Restoran/src/Fitur/ListDataPegawai.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class ListDataPegawai {
-    
+
+    public static void main(String[] args) {
+        Scanner mn = new Scanner(System.in);
+        char awal;
+
+        System.out.println("***** Menu List Data Pegawai *****\n");
+        System.out.println("====================================================================================================================");
+        System.out.println("| Pegawai Satu\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+        System.out.println("| Pegawai Dua\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+        System.out.println("| Pegawai Tiga\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+        System.out.println("| Pegawai Empat\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+        System.out.println("| Pegawai Lima\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+        System.out.println("====================================================================================================================");
+
+
+        System.out.println("\nKembali ke menu awal? (y/n)");
+        awal = mn.next().charAt(0);
+
+    }
 }
