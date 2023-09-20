6558ed18f616f0f2fe7829e75a48b46dca97e5f3
Gayuh <gayuhcakrawala@gmail.com>
2023-09-20 12:48:15 +0700

    Delete sesuatu

--- Pegawai Restoran/src/Fitur/TambahPegawai.java
+++ Pegawai Restoran/src/Fitur/TambahPegawai.java
@@ -1,70 +1,47 @@
 import java.util.Scanner;
 
 public class TambahPegawai {
-public static void main(String[] args) {
-    while (true) {
-    
-    Scanner pg = new Scanner(System.in);
-    Scanner rl = new Scanner(System.in);
-    Scanner mn = new Scanner(System.in);
-    
-    String namaPegawai, domisili, pendidikan, penyakit, role="";
-    char back;
-    int pilihRole;
-    System.out.println("****Menu Menambah Data Pegawai****");
-    
-    System.out.println("\nMasukkan nama pegawai : ");
-    namaPegawai = pg.nextLine();
-   
-    System.out.println("\nPilih Role : ");
-    System.out.println("1. Role 1");
-    System.out.println("2. Role 2");
-    System.out.println("3. Role 3");
-    System.out.println("4. Role 4");
-    pilihRole = rl.nextInt();
-   
- switch (pilihRole) {
-        case 1:
-           role = "Role 1";
-            break;
-        case 2:
-           role = "Role 2";
-            break;
-        case 3:
-           role = "Role 3";
-            break;
-        case 4:
-           role = "Role 4";
-            break;   
-        default:
-            break;
-    }
+    public static void main(String[] args) {
 
-    System.out.println("\nMasukkan Domisili pegawai : ");
-    domisili = pg.nextLine();
-   
-    System.out.println("\nMasukkan Riwayat Pendidikan : ");
-    pendidikan = pg.nextLine();
-    System.out.println("\nMasukkan Riwayat Penyakit : ");
-    penyakit = pg.nextLine();
-    
-    System.out.println("================================");
-    System.out.println("| Nama Pegawai        : " +namaPegawai);
-    System.out.println("| Role                : " +role);
-    System.out.println("| Domisili            : " +domisili);
-    System.out.println("| Riwayat Pendidikan  : " +pendidikan);
-    System.out.println("| Riwayat penyakit    : " +penyakit);
-    System.out.println("================================");
-    
-    System.out.println("\n***Data telah ditambahkan***");
+        Scanner pg = new Scanner(System.in);
+        Scanner rl = new Scanner(System.in);
+        Scanner mn = new Scanner(System.in);
 
-    System.out.println("\nKembali ke menu awal? (y/n)");
-    back = mn.next().charAt(0);
-     if (back=='y') {
-        System.out.println("Kembali ke menu"); break;
-    } else {
-        continue;
+        String namaPegawai, domisili, pendidikan, penyakit, role = "";
+        char back;
+        int pilihRole;
+        System.out.println("****Menu Menambah Data Pegawai****");
+
+        System.out.println("\nMasukkan nama pegawai : ");
+        namaPegawai = pg.nextLine();
+
+        System.out.println("\nPilih Role : ");
+        System.out.println("1. Role 1");
+        System.out.println("2. Role 2");
+        System.out.println("3. Role 3");
+        System.out.println("4. Role 4");
+        pilihRole = rl.nextInt();
+
+        System.out.println("\nMasukkan Domisili pegawai : ");
+        domisili = pg.nextLine();
+
+        System.out.println("\nMasukkan Riwayat Pendidikan : ");
+        pendidikan = pg.nextLine();
+        System.out.println("\nMasukkan Riwayat Penyakit : ");
+        penyakit = pg.nextLine();
+
+        System.out.println("================================");
+        System.out.println("| Nama Pegawai        : " + namaPegawai);
+        System.out.println("| Role                : " + pilihRolerole);
+        System.out.println("| Domisili            : " + domisili);
+        System.out.println("| Riwayat Pendidikan  : " + pendidikan);
+        System.out.println("| Riwayat penyakit    : " + penyakit);
+        System.out.println("================================");
+
+        System.out.println("\n***Data telah ditambahkan***");
+
+        System.out.println("\nKembali ke menu awal? (y/n)");
+        back = mn.next().charAt(0);
+
     }
-}
-}
 }
