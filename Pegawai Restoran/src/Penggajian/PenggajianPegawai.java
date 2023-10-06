fec02998a90a57a47400bdbf869257011cb47e9a
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2023-10-06 13:53:40 +0700

    add List Menu

--- Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
+++ Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
@@ -27,6 +27,54 @@ public class PenggajianPegawai {
 
         if (enteredUsername.equals(username) && enteredPassword.equals(password)) {
             System.out.println("\n\t\t\t      Welcome to Sistem Pegawai, " + username + "!");
+
+            // List Menu
+            System.out.println("=========================================");
+            System.out.println("           MENU SISTEM KARYAWAN          ");
+            System.out.println("=========================================");
+            System.out.println("1. List Data Karyawan");
+            System.out.println("2. Tambah Data Karyawan");
+            System.out.println("3. Ubah Data Karyawan");
+            System.out.println("4. Hapus Data Karyawan");
+            System.out.println("5. Sistem Gaji");
+            System.out.println("=========================================");
+            System.out.print("   Pilih menu yang anda inginkan (1-5) : ");
+
+            pilihMenu = inputText.next().charAt(0);
+            switch (pilihMenu) {
+                case '1':
+                    System.out.println("\n***** Menu List Data Pegawai *****");
+                    System.out.println("=========================================");
+                    System.out.println("\t    Work In Progress");
+                    System.out.println("=========================================");
+                    break;
+                case '2':
+                    System.out.println("\n***** Menu Tambah Data Pegawai *****");
+                    System.out.println("=========================================");
+                    System.out.println("\t    Work In Progress");
+                    System.out.println("=========================================");
+                    break;
+                case '3':
+                    System.out.println("\n***** Menu Ubah Data Pegawai *****");
+                    System.out.println("=========================================");
+                    System.out.println("\t    Work In Progress");
+                    System.out.println("=========================================");
+                    break;
+                case '4':
+                    System.out.println("\n***** Menu Hapus Data Karyawan ****");
+                    System.out.println("=========================================");
+                    System.out.println("\t    Work In Progress");
+                    System.out.println("=========================================");
+                    break;
+                case '5':
+                    System.out.println("\n***** Menu Sistem Gaji *****");
+                    System.out.println("=========================================");
+                    System.out.println("\t    Work In Progress");
+                    System.out.println("=========================================");
+                    break;
+                default:
+                    System.out.println("\nMenu yang anda pilih tidak ditemukan! Mohon re-run codingan anda");
+            }
         } else {
             System.out.println("\n\t\t\t  Maaf, username atau password anda salah!");
         }
