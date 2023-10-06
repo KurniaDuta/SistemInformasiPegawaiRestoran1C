386acee657aa4a1293ca0d6ee4d03e240f55a57e
Duta Kurnia A <dutaka2945@gmail.com>
2023-10-06 13:46:07 +0700

    Update Inisialisasi Variabel

--- Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
+++ Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class PenggajianPegawai {
     public static void main(String[] args) {
 
-        //Inisialisasi Scanner
+        // Inisialisasi Scanner
         Scanner inputText = new Scanner(System.in);
         Scanner inputNum = new Scanner(System.in);
 
-        //Inisialisasi variabel
-        String username, password;
-        char pilihMenu, pilihRole;
-        int gajiPokok;
+        // Inisialisasi variabel
+        String username = "admin1", password = "admin1", namaPegawai, domisili, pendidikan, penyakit, role;
+        char pilihMenu, pilihRole, back;
+        int gajiPokok = 4000000, jmlIzin, nominalIzin = 100000, gajiAkhir, potIzin,
+                intensifLembur = 25000, jmlLembur,
+                gajiLembur, shiftMasuk;
+
+        // Login
+        
 
     }
 }
