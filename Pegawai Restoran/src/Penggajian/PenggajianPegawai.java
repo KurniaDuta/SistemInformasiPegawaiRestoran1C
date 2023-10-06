b3f3ffe57703c7e711e255a8e5c4484d3ce3d7cf
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2023-10-06 13:36:22 +0700

    Inisialisasi Variabel & Scanner

--- Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
+++ Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
@@ -1,7 +1,18 @@
 package Penggajian;
 
+import java.util.Scanner;
+
 public class PenggajianPegawai {
     public static void main(String[] args) {
-        
+
+        //Inisialisasi Scanner
+        Scanner inputText = new Scanner(System.in);
+        Scanner inputNum = new Scanner(System.in);
+
+        //Inisialisasi variabel
+        String username, password;
+        char pilihMenu, pilihRole;
+        int gajiPokok;
+
     }
 }
