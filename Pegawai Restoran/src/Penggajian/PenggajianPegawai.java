15e9337b638c1345ce5b374e18c54f9c07b27211
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2023-10-16 20:06:03 +0700

    Add loop pilih menu

--- Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
+++ Pegawai Restoran/src/Penggajian/PenggajianPegawai.java
@@ -32,142 +32,146 @@ public class PenggajianPegawai {
                 System.out.println("\n\t\t\t      Welcome to Sistem Pegawai, " + enteredUsername + "!");
 
                 // List Menu
-                System.out.println(
-                        "\n===============================================================================================");
-                System.out.println("\t\t\t\tMENU SISTEM PEGAWAI RESTORAN 1C");
-                System.out.println(
-                        "===============================================================================================");
-                System.out.println("\t\t\t\t1. List Data Pegawai");
-                System.out.println("\t\t\t\t2. Tambah Data Pegawai");
-                System.out.println("\t\t\t\t3. Ubah Data Pegawai");
-                System.out.println("\t\t\t\t4. Hapus Data Pegawai");
-                System.out.println("\t\t\t\t5. Penggajian Pegawai");
-                System.out.println(
-                        "===============================================================================================");
-                System.out.print("\t\t\t   Pilih menu yang anda inginkan (1-5) : ");
+                do {
+                    System.out.println(
+                            "\n===============================================================================================");
+                    System.out.println("\t\t\t\tMENU SISTEM PEGAWAI RESTORAN 1C");
+                    System.out.println(
+                            "===============================================================================================");
+                    System.out.println("\t\t\t\t1. List Data Pegawai");
+                    System.out.println("\t\t\t\t2. Tambah Data Pegawai");
+                    System.out.println("\t\t\t\t3. Ubah Data Pegawai");
+                    System.out.println("\t\t\t\t4. Hapus Data Pegawai");
+                    System.out.println("\t\t\t\t5. Penggajian Pegawai");
+                    System.out.println(
+                            "===============================================================================================");
+                    System.out.print("\t\t\t   Pilih menu yang anda inginkan (1-5) : ");
 
-                pilihMenu = inputNum.next().charAt(0);
-                switch (pilihMenu) {
-                    case '1':
-                        // Data Pegawai
-                        System.out.println("***** Menu List Data Pegawai *****\n");
-                        System.out.println(
-                                "====================================================================================================================");
-                        System.out.println(
-                                "| Pegawai Satu\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
-                        System.out.println(
-                                "| Pegawai Dua\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
-                        System.out.println(
-                                "| Pegawai Tiga\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
-                        System.out.println(
-                                "| Pegawai Empat\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
-                        System.out.println(
-                                "| Pegawai Lima\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
-                        System.out.println(
-                                "====================================================================================================================");
-                        break;
-                    case '2':
+                    pilihMenu = inputNum.next().charAt(0);
+                    switch (pilihMenu) {
+                        case '1':
+                            // Data Pegawai
+                            System.out.println("***** Menu List Data Pegawai *****\n");
+                            System.out.println(
+                                    "====================================================================================================================");
+                            System.out.println(
+                                    "| Pegawai Satu\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+                            System.out.println(
+                                    "| Pegawai Dua\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+                            System.out.println(
+                                    "| Pegawai Tiga\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+                            System.out.println(
+                                    "| Pegawai Empat\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+                            System.out.println(
+                                    "| Pegawai Lima\t|\tRole\t|\tDomisili\t|\tRiwayat Penyakit\t|\tRiwayat Pendidikan |");
+                            System.out.println(
+                                    "====================================================================================================================");
+                            break;
+                        case '2':
 
-                        // Tambah Data
-                        System.out.println("****Menu Menambah Data Pegawai****");
+                            // Tambah Data
+                            System.out.println("****Menu Menambah Data Pegawai****");
 
-                        System.out.println("\nMasukkan nama pegawai : ");
-                        namaPegawai = inputText.nextLine();
+                            System.out.println("\nMasukkan nama pegawai : ");
+                            namaPegawai = inputText.nextLine();
 
-                        System.out.println("\nPilih Role : ");
-                        System.out.println("1. Role 1");
-                        System.out.println("2. Role 2");
-                        System.out.println("3. Role 3");
-                        System.out.println("4. Role 4");
-                        pilihRole = inputNum.nextInt();
+                            System.out.println("\nPilih Role : ");
+                            System.out.println("1. Role 1");
+                            System.out.println("2. Role 2");
+                            System.out.println("3. Role 3");
+                            System.out.println("4. Role 4");
+                            pilihRole = inputNum.nextInt();
 
-                        System.out.println("\nMasukkan Domisili pegawai : ");
-                        domisili = inputText.nextLine();
+                            System.out.println("\nMasukkan Domisili pegawai : ");
+                            domisili = inputText.nextLine();
 
-                        System.out.println("\nMasukkan Riwayat Pendidikan : ");
-                        pendidikan = inputText.nextLine();
-                        System.out.println("\nMasukkan Riwayat Penyakit : ");
-                        penyakit = inputText.nextLine();
+                            System.out.println("\nMasukkan Riwayat Pendidikan : ");
+                            pendidikan = inputText.nextLine();
+                            System.out.println("\nMasukkan Riwayat Penyakit : ");
+                            penyakit = inputText.nextLine();
 
-                        switch (pilihRole) {
-                            case 1:
-                                role = "Role 1";
-                                break;
-                            case 2:
-                                role = "Role 2";
-                                break;
-                            case 3:
-                                role = "Role 3";
-                                break;
-                            case 4:
-                                role = "Role 4";
-                                break;
-                            default:
-                                role = "Role Invalid";
-                                break;
-                        }
+                            switch (pilihRole) {
+                                case 1:
+                                    role = "Role 1";
+                                    break;
+                                case 2:
+                                    role = "Role 2";
+                                    break;
+                                case 3:
+                                    role = "Role 3";
+                                    break;
+                                case 4:
+                                    role = "Role 4";
+                                    break;
+                                default:
+                                    role = "Role Invalid";
+                                    break;
+                            }
 
-                        System.out.println("================================");
-                        System.out.println("| Nama Pegawai        : " + namaPegawai);
-                        System.out.println("| Role                : " + role);
-                        System.out.println("| Domisili            : " + domisili);
-                        System.out.println("| Riwayat Pendidikan  : " + pendidikan);
-                        System.out.println("| Riwayat penyakit    : " + penyakit);
-                        System.out.println("================================");
+                            System.out.println("================================");
+                            System.out.println("| Nama Pegawai        : " + namaPegawai);
+                            System.out.println("| Role                : " + role);
+                            System.out.println("| Domisili            : " + domisili);
+                            System.out.println("| Riwayat Pendidikan  : " + pendidikan);
+                            System.out.println("| Riwayat penyakit    : " + penyakit);
+                            System.out.println("================================");
 
-                        System.out.println("\n***Data telah ditambahkan***");
+                            System.out.println("\n***Data telah ditambahkan***");
 
-                        break;
-                    case '3':
-                        System.out.println("\n***** Menu Ubah Data Pegawai *****");
-                        System.out.println("=========================================");
-                        System.out.println("\t    Work In Progress");
-                        System.out.println("=========================================");
-                        break;
-                    case '4':
-                        System.out.println("\n***** Menu Hapus Data Pegawai ****");
-                        System.out.println("=========================================");
-                        System.out.println("\t    Work In Progress");
-                        System.out.println("=========================================");
-                        break;
-                    case '5':
+                            break;
+                        case '3':
+                            System.out.println("\n***** Menu Ubah Data Pegawai *****");
+                            System.out.println("=========================================");
+                            System.out.println("\t    Work In Progress");
+                            System.out.println("=========================================");
+                            break;
+                        case '4':
+                            System.out.println("\n***** Menu Hapus Data Pegawai ****");
+                            System.out.println("=========================================");
+                            System.out.println("\t    Work In Progress");
+                            System.out.println("=========================================");
+                            break;
+                        case '5':
 
-                        // Penggajian Pegawai
-                        System.out.println("======================================");
-                        System.out.println("          Penggajian Pegawai         ");
-                        System.out.println("======================================");
-                        System.out.print("Masukkan Nama Pegawai: ");
-                        namaPegawai = inputText.nextLine();
-                        System.out.print("Masukkan jumlah izin (shift) : ");
-                        jmlIzin = inputNum.nextInt();
-                        System.out.print("Masukkan jumlah lembur (jam): ");
-                        jmlLembur = inputNum.nextInt();
+                            // Penggajian Pegawai
+                            System.out.println("======================================");
+                            System.out.println("          Penggajian Pegawai         ");
+                            System.out.println("======================================");
+                            System.out.print("Masukkan Nama Pegawai: ");
+                            namaPegawai = inputText.nextLine();
+                            System.out.print("Masukkan jumlah izin (shift) : ");
+                            jmlIzin = inputNum.nextInt();
+                            System.out.print("Masukkan jumlah lembur (jam): ");
+                            jmlLembur = inputNum.nextInt();
 
-                        shiftMasuk = 30 - jmlIzin;
-                        potIzin = jmlIzin * nominalIzin;
-                        gajiLembur = jmlLembur * intensifLembur;
-                        gajiAkhir = gajiPokok - potIzin + gajiLembur;
+                            shiftMasuk = 30 - jmlIzin;
+                            potIzin = jmlIzin * nominalIzin;
+                            gajiLembur = jmlLembur * intensifLembur;
+                            gajiAkhir = gajiPokok - potIzin + gajiLembur;
 
-                        System.out.println("\n\n\n==============================================================");
-                        System.out.println("                 Slip Gaji Pegawai Restoran 1C                ");
-                        System.out.println("==============================================================\n");
-                        System.out.println("Nama Pegawai\t: " + namaPegawai);
-                        System.out.println("Shift Masuk\t: " + shiftMasuk + " shift");
-                        System.out.println("Shift Izin\t: " + jmlIzin + " shift");
-                        System.out.println("Lembur\t\t: " + jmlLembur + " jam\n");
-                        System.out.println("==============================================================\n");
-                        System.out.println("Gaji Pokok\t: " + gajiPokok);
-                        System.out.println("Potongan izin\t: " + potIzin);
-                        System.out.println("Gaji Lembur\t: " + gajiLembur + "\n");
-                        System.out.println("==============================================================\n");
-                        System.out.println("Total Gaji Akhir: " + "Rp " + gajiAkhir + "\n");
-                        System.out.println("==============================================================");
+                            System.out.println("\n\n\n==============================================================");
+                            System.out.println("                 Slip Gaji Pegawai Restoran 1C                ");
+                            System.out.println("==============================================================\n");
+                            System.out.println("Nama Pegawai\t: " + namaPegawai);
+                            System.out.println("Shift Masuk\t: " + shiftMasuk + " shift");
+                            System.out.println("Shift Izin\t: " + jmlIzin + " shift");
+                            System.out.println("Lembur\t\t: " + jmlLembur + " jam\n");
+                            System.out.println("==============================================================\n");
+                            System.out.println("Gaji Pokok\t: " + gajiPokok);
+                            System.out.println("Potongan izin\t: " + potIzin);
+                            System.out.println("Gaji Lembur\t: " + gajiLembur + "\n");
+                            System.out.println("==============================================================\n");
+                            System.out.println("Total Gaji Akhir: " + "Rp " + gajiAkhir + "\n");
+                            System.out.println("==============================================================");
 
-                        break;
-                    default:
-                        System.out.println("\nMenu yang anda pilih tidak ditemukan! Mohon re-run codingan anda");
-                }
-                break;
+                            break;
+                        default:
+                            System.out.println("\nMenu yang anda pilih tidak ditemukan! Mohon re-run codingan anda");
+                    }
+                    System.out.println("Apakah anda ingin kembali ke menu? (y/n)");
+                    back = inputNum.next().charAt(0);
+                } while (back == 'y' || back == 'Y');
+                
             } else {
                 System.out.println("\n\t\t\t  Login Gagal. " + (3 - attempt) + " kali percobaan lagi");
                 if (attempt == 3) {
